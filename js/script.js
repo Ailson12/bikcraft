758f377c928330a3762a496c298fe32a5aa42b23
Ailson <robertofeitosa06@gmail.com>
2022-09-15 21:04:06 -0400

    adicionando perguntas frequentes

--- js/script.js
+++ js/script.js
@@ -4,7 +4,29 @@ links.forEach((link) => {
   const href = link.href;
 
   if (url.includes(href)) {
-    link.classList.add("ativo")
+    link.classList.add("ativo");
   }
-})
-console.log(links)
+});
+
+// ativar items do orçamento
+const parametros = new URLSearchParams(location.search);
+parametros.forEach((parametro) => {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+});
+
+// Perguntas frequentes
+const perguntas = document.querySelectorAll(".perguntas button");
+perguntas.forEach((pergunta) => {
+  pergunta.addEventListener("click", (event) => {
+    const button = event.currentTarget;
+    const controls = button.getAttribute("aria-controls");
+    const resposta = document.getElementById(controls);
+
+    resposta.classList.toggle("ativo");
+    const ativo = resposta.classList.contains("ativo");
+    button.setAttribute("aria-expanded", ativo);
+  });
+});
